2d735e73ccb684d3333745284aa018d01b88e927
Miguel Muscat <miguelmuscat93@gmail.com>
2017-10-03 13:03:16 +0200

    Added `IntervalAwareInterface`

---
+++ src/IntervalAwareInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace RebelCode\Time;
+
+/**
+ * Something that is aware of, and can provide, an interval.
+ *
+ * @since [*next-version*]
+ */
+interface IntervalAwareInterface
+{
+    /**
+     * Retrieves the interval associated with this instance.
+     *
+     * @since [*next-version*]
+     *
+     * @return IntervalInterface The interval.
+     */
+    public function getInterval();
+}
